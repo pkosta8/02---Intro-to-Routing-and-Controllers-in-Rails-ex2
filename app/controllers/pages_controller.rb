64151ae6f844580b95b508617b327708cb2f5ddc
paulcosta2001@gmail.com <paulcosta2001@gmail.com>
2018-01-10 16:47:10 -0500

    final header

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,15 @@
 class PagesController < ApplicationController
 
-def welcome
-
-end
-
+  def welcome
+    @header = "This is the welcome page"
+  end
 
 def about
+  @header = "This is the about page"
 end
 
 def contest
+  @header = "This is the contest page"
 end
 
 end
